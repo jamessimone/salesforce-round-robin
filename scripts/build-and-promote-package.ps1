157effccc76bed0b8fbced209fb13839d418528e
james simone <jamirus99@gmail.com>
2022-02-28 22:39:57 -0700

    Started adding in javadoc comments in a few places where it will help Apex consumers of the code. Removed a few lingering references to apex-rollup from copied files. Added definitionFile property to sfdx-project.json, though packaging still fails locally when attempting to revert AbstractCacheRepo to non-safeguarded version

--- scripts/build-and-promote-package.ps1
+++ scripts/build-and-promote-package.ps1
@@ -13,17 +13,13 @@ function Get-Current-Git-Branch() {
 
 function Start-Package-Promotion {
   Write-Debug "Beginning promote script"
-
-  $allReadmes = Get-ChildItem -Exclude node_modules, .sfdx, tests | Get-ChildItem -Filter README.md -Recurse
-  foreach ($readme in $allReadmes) {
-    $readmePackageIdResults = (Select-String -Path $readme 'https:\/\/login.salesforce.com\/packaging\/installPackage.apexp\?p0=.{0,18}')
-    if ($readmePackageIdResults.Matches.Length -gt 0) {
-      $packageIdSplit = $readmePackageIdResults.Matches[0].Value.Split("=")
-      if ($packageIdSplit.Length -eq 2) {
-        $packageId = $packageIdSplit[1]
-        Write-Debug "Promoting $packageId from $readme"
-        npx sfdx force:package:version:promote -p $packageId -n
-      }
+  $readmePackageIdResults = (Select-String -Path "README.md" 'https:\/\/login.salesforce.com\/packaging\/installPackage.apexp\?p0=.{0,18}')
+  if ($readmePackageIdResults.Matches.Length -gt 0) {
+    $packageIdSplit = $readmePackageIdResults.Matches[0].Value.Split("=")
+    if ($packageIdSplit.Length -eq 2) {
+      $packageId = $packageIdSplit[1]
+      Write-Debug "Promoting $packageId from $readme"
+      npx sfdx force:package:version:promote -p $packageId -n
     }
   }
   Write-Debug "Finished package promotion!"
