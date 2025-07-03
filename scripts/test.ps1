7a37a497a9de49d43def5653716324b2cfbd8fe5
James Simone <16430727+jamessimone@users.noreply.github.com>
2025-07-03 17:37:31 -0400

    Further pipeline work

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,7 +2,7 @@ $DebugPreference = 'Continue'
 $ErrorActionPreference = 'Stop'
 # This is also the same script that runs on Github via the Github Action configured in .github/workflows - there, the
 # DEVHUB_SFDX_URL.txt file is populated in a build step
-$testInvocation = 'npx sf apex run test --suite-names -s RoundRobinTestSuite --result-format human --wait 20 --code-coverage --output-dir ./tests/apex --concise'
+$testInvocation = 'npx sf apex run test --suite-names RoundRobinTestSuite --result-format human --wait 20 --code-coverage --output-dir ./tests/apex --concise'
 $scratchOrgName = 'round-robin-scratch'
 function Start-Deploy() {
   Write-Debug "Deploying source ..."
@@ -22,7 +22,7 @@ function Start-Tests() {
 
   try {
     Write-Debug "Deleting scratch org ..."
-    sf org delete scratch --no-prompt --target-org $scratchOrgName
+    npx sf org delete scratch --no-prompt --target-org "$scratchOrgName"
   } catch {
     Write-Debug "Scratch org deletion failed, continuing ..."
   }
@@ -43,7 +43,7 @@ if($scratchOrgAllotment -gt 0) {
   try {
     Write-Debug "Beginning scratch org creation"
     # Create Scratch Org
-    npx sf org create scratch --definition-file config/project-scratch-def.json --alias $scratchOrgName --set-default --duration-days 30
+    npx sf org create scratch --definition-file config/project-scratch-def.json --alias "$scratchOrgName" --set-default --duration-days 30
   } catch {
     # Do nothing, we'll just try to deploy to the Dev Hub instead
   }
